7f433bdad6a870672861cb7e7a83d60457ab8c06
jky520 <jky1988@qq.com>
2017-08-15 11:55:30 +0800

    Initial commit

---
+++ src/main/java/com/jky/recog/RecogApplication.java
@@ -0,0 +1,12 @@
+package com.jky.recog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecogApplication.class, args);
+	}
+}
